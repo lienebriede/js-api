60990722dd1166d10786d9ba03286df23f22c4f0
Liene Briede <lienesjaunkundze@gmail.com>
2024-02-21 15:15:06 +0000

    Add display data function

--- assets/js/script.js
+++ assets/js/script.js
@@ -3,6 +3,8 @@ const API_URL = "https://ci-jshint.herokuapp.com/api";
 const resultsModal = new bootstrap.Modal(document.getElementById("resultsModal"));
 
 document.getElementById("status").addEventListener("click", e => getStatus(e));
+
+
 /* To get status we need to GET it and then display it
 For Get we use the sentence that is shown in JSHint APIs instructions
 */
@@ -13,13 +15,35 @@ async function getStatus(e) {
     const response = await fetch(queryString);
 
     const data = await response.json();
-
+    /* If want to display in console.log only expiry date, then add
+    console.log(data.expiry) instead of data
+    Here in dusplay function later it has been added {data.expiry} */
     if (response.ok) {
-        console.log(data.expiry);
+        displayStatus(data);
+    } else {
+        throw new Error(data.error);
     }
 
 }
 
+function displayStatus(data) {
 
+    /*
+    In this way it makes also the sentence in two pieces:
+    
+    let heading = "API Key Status";
+    let results = `<div>Your key is valid until</div>`;
+    results += `<div class="key-status">${data.expiry}</div>`;
+
+    document.getElementById("resultsModalTitle").innerText = heading;
+    document.getElementById("results-content").innerHTML = results;
+    resultsModal.show();
+    
+    */
+
+    document.getElementById("resultsModalTitle").innerText = "API Key Status";
+    document.getElementById("results-content").innerHTML = `Your key is valid until${data.expiry}`;
+    resultsModal.show();
+}
 
 
